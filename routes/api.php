f26aed53e710ec0d59c4522633d10631b599c449
Rahat Baksh <rahat392@gmail.com>
2024-05-23 16:36:31 +0600

    adding route prefix

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Http\Request;
 use Rahat1994\SparkcommerceRestRoutes\Http\Controllers\AuthController;
- 
-Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['prefix' => 'sc/v1'], function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
+// Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/register', [AuthController::class, 'register']);
