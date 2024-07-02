bd15c417483f3d11f86ffa19483f7a269c29be9a
Rahat Baksh <rahat392@gmail.com>
2024-07-02 14:41:55 +0600

    updating SCProduct resource to include image_gallery and product_image

--- src/Http/Resources/SCProductResource.php
+++ src/Http/Resources/SCProductResource.php
@@ -13,7 +13,11 @@ class SCProductResource extends JsonResource
             'name' => $this->name,
             'slug' => $this->slug,
             'categories' => SCCategoryResource::collection($this->categories),
-            'product_image' => 'https://fastly.picsum.photos/id/63/5000/2813.jpg',
+            'product_image' => ($this->hasMedia('product_image'))?$this->getMedia('product_image')->first()->getUrl() : 'https://fastly.picsum.photos/id/63/5000/2813.jpg',
+            'description' => $this->description,
+            'gallery' =>($this->hasMedia('product_image_gallery'))? $this->getMedia('product_image_gallery')->map(function ($item) {
+                return $item->getUrl();
+            }) : [],
             'rating' => [
                 'average' => 4.5,
                 'total' => 100
