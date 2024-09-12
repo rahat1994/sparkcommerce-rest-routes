003ee49f05a212d25e75367e250c0e62f68b22e3
Rahat Baksh <rahat392@gmail.com>
2024-09-13 02:02:16 +0600

    Refactor getRecordBySlug method to accept an optional recordModel parameter

--- src/Concerns/CanInteractWithRecord.php
+++ src/Concerns/CanInteractWithRecord.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace Rahat1994\SparkcommerceRestRoutes\Concerns;
+
 use Illuminate\Database\Eloquent\Collection;
 
 trait CanInteractWithRecord
 {
     public $recordModel;
-    protected function getRecordBySlug(string $slug)
+    protected function getRecordBySlug(string $slug, string $recordModel = null)
     {
-        return $this->recordModel::where('slug', $slug)->firstOrFail();
+        $recordModel = $recordModel ?? $this->recordModel;
+        return $recordModel::where('slug', $slug)->firstOrFail();
     }
 
     protected function getRecordsByItemTypeAndSlugs(string $itemType, array $slugs): Collection
@@ -19,7 +21,6 @@ trait CanInteractWithRecord
     protected function getRecordsWithRelationShip(array $relations): Collection
     {
         return $this->recordModel::with($relations)->get();
-
     }
 
     protected function getRecordsWhere(array $conditions): Collection
